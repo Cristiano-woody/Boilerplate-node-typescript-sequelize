73a90f81348e2169883c8dba87517a40e52e4b9c
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-04-24 21:54:10 -0300

    bild: cors instalation and configuration

--- src/api/app.ts
+++ src/api/app.ts
@@ -3,12 +3,18 @@ import express from 'express'
 import MainRoutes from './routes/mainRoutes'
 // importando .env
 import * as dotenv from 'dotenv'
+// importando cors
+import cors from 'cors'
 
 dotenv.config()
 
 // iniciando o app
 const app = express()
 
+// usando cors
+app.use(cors())
+
+//
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
